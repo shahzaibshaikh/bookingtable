0dc75937deb68eceda91ad9105a8f4dbda5a1af4
Shahzaib Shaikh <shahzaibshaikh98@gmail.com>
2023-05-01 13:06:08 +0500

    📦 NEW: added reservation page

--- app/restaurant/[slug]/menu/page.tsx
+++ app/restaurant/[slug]/menu/page.tsx
@@ -8,7 +8,7 @@ function RestaurantMenu() {
         <nav className='bg-white p-2 flex justify-between'>
           <a href='' className='font-bold text-gray-700 text-2xl'>
             {' '}
-            OpenTable{' '}
+            BookingTable{' '}
           </a>
           <div>
             <div className='flex'>
