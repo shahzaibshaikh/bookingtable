1a968ea4334075c51d2ae15c5cb7d6494d5a5a67
Shahzaib Shaikh <shahzaibshaikh98@gmail.com>
2023-05-01 12:57:30 +0500

    👌 IMPROVE: added search index route XML

--- app/page.tsx
+++ app/page.tsx
@@ -8,7 +8,7 @@ export default function Home() {
         <nav className='bg-white p-2 flex justify-between'>
           <a href='' className='font-bold text-gray-700 text-2xl'>
             {' '}
-            OpenTable{' '}
+            BookingTable{' '}
           </a>
           <div>
             <div className='flex'>
