b45da03832ecd76bdf081e4151233fc611d3f0fb
Shahzaib Shaikh <shahzaibshaikh98@gmail.com>
2023-05-01 12:35:15 +0500

    👌 IMPROVE: added tailwind css

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import styles from './page.module.css';
 export default function Home() {
   return (
     <main className={styles.main}>
-      <h1>Hello, this is first Next.js project.</h1>
+      <h1 className='text-red-400 text-lg'>Hello, this is first Next.js project.</h1>
     </main>
   );
 }
