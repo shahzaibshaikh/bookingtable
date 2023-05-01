6b8ab01be8f60de4f7801ed7a4677a38cb694a5e
Shahzaib Shaikh <shahzaibshaikh98@gmail.com>
2023-05-01 21:15:17 +0500

    🐛 FIX: change import of useRouter

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,13 @@
 'use client';
 
 import Link from 'next/link';
-import { useRouter } from 'next/router';
+import { useRouter } from 'next/navigation';
 import { useState } from 'react';
 
 export default function Home() {
   const router = useRouter();
   const [location, setLocation] = useState('');
+
   return (
     <main className='bg-gray-100 min-h-screen w-screen'>
       <main className='max-w-screen-2xl m-auto bg-white'>
@@ -41,7 +42,13 @@ export default function Home() {
                   value={location}
                   onChange={e => setLocation(e.target.value)}
                 />
-                <button className='rounded bg-red-600 px-9 py-2 text-white'>
+                <button
+                  className='rounded bg-red-600 px-9 py-2 text-white'
+                  onClick={() => {
+                    if (location === 'banana') return;
+                    router.push('/search');
+                  }}
+                >
                   Let's go
                 </button>
               </div>
