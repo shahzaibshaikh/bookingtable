a5bacb8843f5f8af8862d52d341b36ff5e86efb9
Shahzaib Shaikh <shahzaibshaikh98@gmail.com>
2023-05-02 20:51:45 +0500

    👌 IMPROVE: changed description of reservation

--- app/reserve/[slug]/page.tsx
+++ app/reserve/[slug]/page.tsx
@@ -67,7 +67,7 @@ function Reserve() {
               <p className='mt-4 text-sm'>
                 By clicking “Complete reservation” you agree to the OpenTable Terms of Use
                 and Privacy Policy. Standard text message rates may apply. You may opt out
-                of receiving text messages at any time.
+                of receiving text messages at.
               </p>
             </div>
           </div>
