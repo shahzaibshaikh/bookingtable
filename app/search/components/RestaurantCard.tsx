0bc93e6da33df24261ca559c7520ecf51cfa0343
Shahzaib Shaikh <shahzaibshaikh98@gmail.com>
2023-05-04 11:24:52 +0500

    👌 IMPROVE: added navigation to search card

--- app/search/components/RestaurantCard.tsx
+++ app/search/components/RestaurantCard.tsx
@@ -1,3 +1,4 @@
+import Link from 'next/link';
 import React from 'react';
 
 function RestaurantCard() {
@@ -22,7 +23,7 @@ function RestaurantCard() {
           </div>
         </div>
         <div className='text-red-600'>
-          <a href=''>View more information</a>
+          <Link href='/restaurant/milestones-stones'>View more information</Link>
         </div>
       </div>
     </div>
