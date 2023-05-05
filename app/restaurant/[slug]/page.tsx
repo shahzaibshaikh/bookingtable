ef9bfa6cd4561a99a531b92e213752691c89cc26
Shahzaib Shaikh <shahzaibshaikh98@gmail.com>
2023-05-05 22:09:59 +0500

    👌 IMPROVE: corrected text

--- app/restaurant/[slug]/page.tsx
+++ app/restaurant/[slug]/page.tsx
@@ -33,9 +33,9 @@ function RestaurantDetail() {
               <p className='text-lg font-light'>
                 The classics you love prepared with a perfect twist, all served up in an
                 atmosphere that feels just right. That’s the Milestones promise. So,
-                whether you’re celebrating a milestone, making the most of Happy Hour or
-                enjoying brunch with friends, you can be sure that every Milestones
-                experience is a simple and perfectly memorable one.
+                whether you’re a milestone, making the most of Happy Hour or enjoying
+                brunch with friends, you can be sure that every Milestones experience is a
+                simple and perfectly memorable one.
               </p>
             </div>
             {/* DESCRIPTION */} {/* IMAGES */}
