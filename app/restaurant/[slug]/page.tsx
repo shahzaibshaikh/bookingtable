18da2cca1dd7cdaeda29ffb9b60673d7eb13c0a2
Shahzaib Shaikh <shahzaibshaikh98@gmail.com>
2023-05-05 22:10:50 +0500

    👌 IMPROVE: added "prepared"

--- app/restaurant/[slug]/page.tsx
+++ app/restaurant/[slug]/page.tsx
@@ -31,11 +31,11 @@ function RestaurantDetail() {
             {/* RATING */} {/* DESCRIPTION */}
             <div className='mt-4'>
               <p className='text-lg font-light'>
-                The classics you love with a perfect twist, all served up in an atmosphere
-                that feels just right. That’s the Milestones promise. So, whether you’re
-                celebrating a milestone, making the most of Happy Hour or enjoying brunch
-                with friends, you can be sure that every Milestones experience is a simple
-                and perfectly memorable one.
+                The classics you love prepared with a perfect twist, all served up in an
+                atmosphere that feels just right. That’s the Milestones promise. So,
+                whether you’re celebrating a milestone, making the most of Happy Hour or
+                enjoying brunch with friends, you can be sure that every Milestones
+                experience is a simple and perfectly memorable one.
               </p>
             </div>
             {/* DESCRIPTION */} {/* IMAGES */}
