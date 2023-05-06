760b796ab02a365e7a9fac5ade25538fc95dd4db
Shahzaib Shaikh <shahzaibshaikh98@gmail.com>
2023-05-06 11:30:16 +0500

    👌 IMPROVE: refactored description component

--- app/restaurant/[slug]/page.tsx
+++ app/restaurant/[slug]/page.tsx
@@ -4,6 +4,7 @@ import Header from './components/Header';
 import RestaurantNavbar from './components/RestaurantNavbar';
 import Title from './components/Title';
 import Rating from './components/Rating';
+import Description from './components/Description';
 
 function RestaurantDetail() {
   return (
@@ -17,15 +18,8 @@ function RestaurantDetail() {
             <RestaurantNavbar />
             <Title />
             <Rating />
-            <div className='mt-4'>
-              <p className='text-lg font-light'>
-                The classics you love prepared with a perfect twist, all served up in an
-                atmosphere that feels just right. That’s the Milestones promise. So,
-                whether you’re celebrating a milestone, making the most of Happy Hour or
-                enjoying brunch with friends, you can be sure that every Milestones
-                experience is a simple and perfectly memorable one.
-              </p>
-            </div>
+            {/* RATING */} {/* DESCRIPTION */}
+            <Description />
             {/* DESCRIPTION */} {/* IMAGES */}
             <div>
               <h1 className='font-bold text-3xl mt-10 mb-7 border-b pb-5'>5 photos</h1>
