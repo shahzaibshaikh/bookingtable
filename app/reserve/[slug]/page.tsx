5f52d73cb27735117d11fd6e03be7be3ac3c71a2
Shahzaib Shaikh <shahzaibshaikh98@gmail.com>
2023-05-06 11:42:28 +0500

    👌 IMPROVE: added reserve page header

--- app/reserve/[slug]/page.tsx
+++ app/reserve/[slug]/page.tsx
@@ -1,6 +1,7 @@
 import Navbar from '@/app/components/Navbar';
 import Link from 'next/link';
 import React from 'react';
+import Header from './components/Header';
 
 function Reserve() {
   return (
@@ -10,26 +11,8 @@ function Reserve() {
         {/* NAVBAR END */}
         <div className='border-t h-screen'>
           <div className='py-9 w-3/5 m-auto'>
-            {/* HEADER */}
-            <div>
-              <h3 className='font-bold'>You're almost done!</h3>
-              <div className='mt-5 flex'>
-                <img
-                  src='https://images.otstatic.com/prod1/49153814/2/medium.jpg'
-                  alt=''
-                  className='w-32 h-18 rounded'
-                />
-                <div className='ml-4'>
-                  <h1 className='text-3xl font-bold'>Aiāna Restaurant Collective</h1>
-                  <div className='flex mt-3'>
-                    <p className='mr-6'>Tues, 22, 2023</p>
-                    <p className='mr-6'>7:30 PM</p>
-                    <p className='mr-6'>3 people</p>
-                  </div>
-                </div>
-              </div>
-            </div>
-            {/* HEADER */} {/* FORM */}
+            <Header />
+            {/* FORM */}
             <div className='mt-10 flex flex-wrap justify-between w-[660px]'>
               <input
                 type='text'
