06b4ce6ad31dc4f63c0e836b6375f8ea1d7fd39f
Shahzaib Shaikh <shahzaibshaikh98@gmail.com>
2023-05-06 11:33:00 +0500

    👌 IMPROVE: refactored reviews component

--- app/restaurant/[slug]/page.tsx
+++ app/restaurant/[slug]/page.tsx
@@ -6,6 +6,7 @@ import Title from './components/Title';
 import Rating from './components/Rating';
 import Description from './components/Description';
 import Images from './components/Images';
+import Reviews from './components/Reviews';
 
 function RestaurantDetail() {
   return (
@@ -21,39 +22,7 @@ function RestaurantDetail() {
             <Rating />
             <Description />
             <Images />
-            {/* REVIEWS */}
-            <div>
-              <h1 className='font-bold text-3xl mt-10 mb-7 borber-b pb-5'>
-                What 100 people are saying
-              </h1>
-              <div>
-                {/* REVIEW CARD */}
-                <div className='border-b pb-7 mb-7'>
-                  <div className='flex'>
-                    <div className='w-1/6 flex flex-col items-center'>
-                      <div className='rounded-full bg-blue-400 w-16 h-16 flex items-center justify-center'>
-                        <h2 className='text-white text-2xl'>MJ</h2>
-                      </div>
-                      <p className='text-center'>Micheal Jordan</p>
-                    </div>
-                    <div className='ml-10 w-5/6'>
-                      <div className='flex items-center'>
-                        <div className='flex mr-5'>*****</div>
-                      </div>
-                      <div className='mt-5'>
-                        <p className='text-lg font-light'>
-                          Laurie was on top of everything! Slow night due to the snow
-                          storm so it worked in our favor to have more one on one with the
-                          staff. Delicious and well worth the money.
-                        </p>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-                {/* REVIEW CARD */}
-              </div>
-            </div>
-            {/* REVIEWS */}
+            <Reviews />
           </div>
           <div className='w-[27%] relative text-reg'>
             <div className='fixed w-[15%] bg-white rounded p-3 shadow'>
