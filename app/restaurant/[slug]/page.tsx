5d69f0b5ecfde1597f9e11bd48210b4ce28882af
Shahzaib Shaikh <shahzaibshaikh98@gmail.com>
2023-05-06 11:31:44 +0500

    👌 IMPROVE: refactored images component

--- app/restaurant/[slug]/page.tsx
+++ app/restaurant/[slug]/page.tsx
@@ -5,6 +5,7 @@ import RestaurantNavbar from './components/RestaurantNavbar';
 import Title from './components/Title';
 import Rating from './components/Rating';
 import Description from './components/Description';
+import Images from './components/Images';
 
 function RestaurantDetail() {
   return (
@@ -18,40 +19,9 @@ function RestaurantDetail() {
             <RestaurantNavbar />
             <Title />
             <Rating />
-            {/* RATING */} {/* DESCRIPTION */}
             <Description />
-            {/* DESCRIPTION */} {/* IMAGES */}
-            <div>
-              <h1 className='font-bold text-3xl mt-10 mb-7 border-b pb-5'>5 photos</h1>
-              <div className='flex flex-wrap'>
-                <img
-                  className='w-56 h-44 mr-1 mb-1'
-                  src='https://resizer.otstatic.com/v2/photos/xlarge/3/41701449.jpg'
-                  alt=''
-                />
-                <img
-                  className='w-56 h-44 mr-1 mb-1'
-                  src='https://resizer.otstatic.com/v2/photos/xlarge/2/41701450.jpg'
-                  alt=''
-                />
-                <img
-                  className='w-56 h-44 mr-1 mb-1'
-                  src='https://resizer.otstatic.com/v2/photos/xlarge/2/41701452.jpg'
-                  alt=''
-                />
-                <img
-                  className='w-56 h-44 mr-1 mb-1'
-                  src='https://resizer.otstatic.com/v2/photos/xlarge/2/41701453.jpg'
-                  alt=''
-                />
-                <img
-                  className='w-56 h-44 mr-1 mb-1'
-                  src='https://resizer.otstatic.com/v2/photos/xlarge/2/41701454.jpg'
-                  alt=''
-                />
-              </div>
-            </div>
-            {/* IMAGES */} {/* REVIEWS */}
+            <Images />
+            {/* REVIEWS */}
             <div>
               <h1 className='font-bold text-3xl mt-10 mb-7 borber-b pb-5'>
                 What 100 people are saying
