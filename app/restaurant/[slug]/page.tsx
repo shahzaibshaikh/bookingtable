fa1982d999db5d6e3c4cd03a98b9e2535082a95e
Shahzaib Shaikh <shahzaibshaikh98@gmail.com>
2023-05-06 11:28:56 +0500

    👌 IMPROVE: refactored rating component

--- app/restaurant/[slug]/page.tsx
+++ app/restaurant/[slug]/page.tsx
@@ -2,6 +2,8 @@ import Navbar from '@/app/components/Navbar';
 import React from 'react';
 import Header from './components/Header';
 import RestaurantNavbar from './components/RestaurantNavbar';
+import Title from './components/Title';
+import Rating from './components/Rating';
 
 function RestaurantDetail() {
   return (
@@ -13,21 +15,8 @@ function RestaurantDetail() {
         <div className='flex m-auto w-2/3 justify-between items-start 0 -mt-11'>
           <div className='bg-white w-[70%] rounded p-3 shadow'>
             <RestaurantNavbar />
-            {/* TITLE */}
-            <div className='mt-4 border-b pb-6'>
-              <h1 className='font-bold text-6xl'>Milesstone Grill</h1>
-            </div>
-            {/* TITLE */} {/* RATING */}
-            <div className='flex items-end'>
-              <div className='ratings mt-2 flex items-center'>
-                <p>*****</p>
-                <p className='text-reg ml-3'>4.9</p>
-              </div>
-              <div>
-                <p className='text-reg ml-4'>600 Reviews</p>
-              </div>
-            </div>
-            {/* RATING */} {/* DESCRIPTION */}
+            <Title />
+            <Rating />
             <div className='mt-4'>
               <p className='text-lg font-light'>
                 The classics you love prepared with a perfect twist, all served up in an
