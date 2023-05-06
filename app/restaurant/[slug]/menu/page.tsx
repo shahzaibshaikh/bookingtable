e667bd8c0011b305b129a0d5889b1d7c7afcdfd4
Shahzaib Shaikh <shahzaibshaikh98@gmail.com>
2023-05-06 11:36:05 +0500

    👌 IMPROVE: refactored header for menu page

--- app/restaurant/[slug]/menu/page.tsx
+++ app/restaurant/[slug]/menu/page.tsx
@@ -1,21 +1,15 @@
 import Navbar from '@/app/components/Navbar';
 import Link from 'next/link';
 import React from 'react';
+import Header from '../components/Header';
 
 function RestaurantMenu() {
   return (
     <main className='bg-gray-100 min-h-screen w-screen'>
       <main className='max-w-screen-2xl m-auto bg-white'>
         <Navbar />
-        {/* HEADER */}
-        <div className='h-96 overflow-hidden'>
-          <div className='bg-center bg-gradient-to-r from-[#0f1f47] to-[#5f6984] h-full flex justify-center items-center'>
-            <h1 className='text-7xl text-white captitalize text-shadow text-center'>
-              Milestones Grill (Toronto)
-            </h1>
-          </div>
-        </div>
-        {/* HEADER */} {/* DESCRIPTION PORTION */}
+        <Header />
+        {/* DESCRIPTION PORTION */}
         <div className='flex m-auto w-2/3 justify-between items-start 0 -mt-11'>
           <div className='bg-white w-[100%] rounded p-3 shadow'>
             {/* RESAURANT NAVBAR */}
--- app/restaurant/[slug]/page.tsx
+++ app/restaurant/[slug]/page.tsx
@@ -15,7 +15,6 @@ function RestaurantDetail() {
       <main className='max-w-screen-2xl m-auto bg-white'>
         <Navbar />
         <Header />
-        {/* DESCRIPTION PORTION */}
         <div className='flex m-auto w-2/3 justify-between items-start 0 -mt-11'>
           <div className='bg-white w-[70%] rounded p-3 shadow'>
             <RestaurantNavbar />
@@ -29,7 +28,6 @@ function RestaurantDetail() {
             <ReservationCard />
           </div>
         </div>
-        {/* DESCRIPTION PORTION */}
       </main>
     </main>
   );
