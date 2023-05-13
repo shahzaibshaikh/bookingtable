e6aa0a4c54e7772662b0215adaff5412d566ea14
Shahzaib Shaikh <shahzaibshaikh98@gmail.com>
2023-05-13 22:51:30 +0500

    👌 IMPROVE: added Price component placeholder

---
+++ app/components/Price.tsx
@@ -0,0 +1,12 @@
+import { PRICE } from '@prisma/client';
+import React from 'react';
+
+interface Props {
+  price: PRICE;
+}
+
+function Price({ price }: Props) {
+  return <div></div>;
+}
+
+export default Price;
