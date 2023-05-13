ba772fec99b513540c46739c3feb9df20ed70120
Shahzaib Shaikh <shahzaibshaikh98@gmail.com>
2023-05-13 17:55:51 +0500

    👌 IMPROVE: added fetchRestaurant async function

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,13 @@
 import Header from './components/Header';
 import RestaurantCard from './components/RestaurantCard';
+import { PrismaClient } from '@prisma/client';
+
+const prisma = new PrismaClient();
+
+const fetchRestaurants = async () => {
+  const restaurants = prisma.restaurant.findMany();
+  return restaurants;
+};
 
 export default function Home() {
   return (
