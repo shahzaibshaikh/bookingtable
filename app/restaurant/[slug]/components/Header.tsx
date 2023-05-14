c16f3f8d2ee0e4c586e6e123451839b8b0ae506c
Shahzaib Shaikh <shahzaibshaikh98@gmail.com>
2023-05-14 19:30:34 +0500

    👌 IMPROVE: made restaurant header dynamic

--- app/restaurant/[slug]/components/Header.tsx
+++ app/restaurant/[slug]/components/Header.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
 
-function Header() {
+function Header({ slug }: { slug: string }) {
   return (
     <div className='h-96 overflow-hidden'>
       <div className='bg-center bg-gradient-to-r from-[#0f1f47] to-[#5f6984] h-full flex justify-center items-center'>
         <h1 className='text-7xl text-white captitalize text-shadow text-center'>
-          Milestones Grill (Toronto)
+          {slug}
         </h1>
       </div>
     </div>
--- app/restaurant/[slug]/layout.tsx
+++ app/restaurant/[slug]/layout.tsx
@@ -1,10 +1,16 @@
-import Header from '@/app/components/Header';
 import React from 'react';
+import Header from './components/Header';
 
-function RestaurantLayout({ children }: { children: React.ReactNode }) {
+function RestaurantLayout({
+  children,
+  params
+}: {
+  children: React.ReactNode;
+  params: { slug: string };
+}) {
   return (
     <main>
-      <Header />
+      <Header name={params.slug} />
       <div className='flex m-auto w-2/3 justify-between items-start 0 -mt-11'>
         {children}
       </div>
