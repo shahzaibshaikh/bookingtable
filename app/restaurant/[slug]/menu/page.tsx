fc551f7878142c092a90e57188ac4376022d1e7e
Shahzaib Shaikh <shahzaibshaikh98@gmail.com>
2023-05-15 20:16:03 +0500

    👌 IMPROVE: passed slug as props

--- app/restaurant/[slug]/menu/page.tsx
+++ app/restaurant/[slug]/menu/page.tsx
@@ -8,7 +8,7 @@ metadata.title = 'Booking Table Menu';
 function RestaurantMenu({ params }: { params: { slug: string } }) {
   return (
     <div className='bg-white w-[100%] rounded p-3 shadow'>
-      <RestaurantNavbar />
+      <RestaurantNavbar slug={params?.slug} />
       <Menu />
     </div>
   );
