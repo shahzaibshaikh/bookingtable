cc2ad98908a0413a110fc1bff1479486be1abb95
Shahzaib Shaikh <shahzaibshaikh98@gmail.com>
2023-05-15 20:16:33 +0500

    👌 IMPROVE: added s in header

--- app/restaurant/[slug]/components/Header.tsx
+++ app/restaurant/[slug]/components/Header.tsx
@@ -5,7 +5,7 @@ function Header({ name }: { name: string }) {
     <div className='h-96 overflow-hidden'>
       <div className='bg-center bg-gradient-to-r from-[#0f1f47] to-[#5f6984] h-full flex justify-center items-center'>
         <h1 className='text-7xl text-white captitalize text-shadow text-center'>
-          {name}
+          {name} s
         </h1>
       </div>
     </div>
