b04e7c1e7a8bfb46984555447feaf9fa155e68b7
Shahzaib Shaikh <shahzaibshaikh98@gmail.com>
2023-05-16 22:07:44 +0500

    🐛 FIX: fixed header component

--- app/restaurant/[slug]/components/Header.tsx
+++ app/restaurant/[slug]/components/Header.tsx
@@ -6,7 +6,7 @@ function Header({ name }: { name: string }) {
       <div className='bg-center bg-gradient-to-r from-[#0f1f47] to-[#5f6984] h-full flex justify-center items-center'>
         <h1 className='text-7xl text-white captitalize text-shadow text-center'>
           {name}
-        </h1
+        </h1>
       </div>
     </div>
   );
