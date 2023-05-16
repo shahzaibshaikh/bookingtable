3db857ca4e5ea2fbdd5999d15818b6836eccf45a
Shahzaib Shaikh <shahzaibshaikh98@gmail.com>
2023-05-16 22:10:40 +0500

    👌 IMPROVE: removed restaurant conditional

--- app/restaurant/[slug]/page.tsx
+++ app/restaurant/[slug]/page.tsx
@@ -42,21 +42,19 @@ async function RestaurantDetail({ params }: { params: { slug: string } }) {
 
   return (
     <>
-      {restaurant && (
-        <>
-          <div className='bg-white w-[70%] rounded p-3 shadow'>
-            <RestaurantNavbar slug={restaurant?.slug} />
-            <Title name={restaurant?.name} />
-            <Rating />
-            <Description description={restaurant?.description} />
-            <Images images={restaurant.images} />
-            <Reviews />
-          </div>
-          <div className='w-[27%] relative text-reg'>
-            <ReservationCard />
-          </div>
-        </>
-      )}
+      <>
+        <div className='bg-white w-[70%] rounded p-3 shadow'>
+          <RestaurantNavbar slug={restaurant?.slug} />
+          <Title name={restaurant?.name} />
+          <Rating />
+          <Description description={restaurant?.description} />
+          <Images images={restaurant.images} />
+          <Reviews />
+        </div>
+        <div className='w-[27%] relative text-reg'>
+          <ReservationCard />
+        </div>
+      </>
     </>
   );
 }
