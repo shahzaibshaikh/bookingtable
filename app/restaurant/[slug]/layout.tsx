afc7eb0db4e8c9d84d77169186c512c8d80bfef0
Shahzaib Shaikh <shahzaibshaikh98@gmail.com>
2023-05-16 22:07:57 +0500

    👌 IMPROVE: changed layout

--- app/restaurant/[slug]/layout.tsx
+++ app/restaurant/[slug]/layout.tsx
@@ -11,9 +11,7 @@ function RestaurantLayout({
   return (
     <main>
       <Header name={params.slug} />
-      <div className='flex m-auto w-2/3 justify-between items-start 0 -mt-11'>
-        {children}
-      </div>
+      <div className='flex m-auto w-2/3 justify-between items-start 0 -mt-11'></div>
     </main>
   );
 }
