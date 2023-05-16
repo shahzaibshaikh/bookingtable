83886aeca6df3a9f2e127744a9fef2bf9b080115
Shahzaib Shaikh <shahzaibshaikh98@gmail.com>
2023-05-16 22:09:29 +0500

    👌 IMPROVE: removed name prop

--- app/restaurant/[slug]/layout.tsx
+++ app/restaurant/[slug]/layout.tsx
@@ -9,7 +9,7 @@ function RestaurantLayout({
 }) {
   return (
     <main>
-      <Header name={params.slug} />
+      <Header />
       <div className='flex m-auto w-2/3 justify-between items-start 0 -mt-11'>
         {children}
       </div>
