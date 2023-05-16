1ec544a1a7dfd073a3987632fbfba1d8348a1213
Shahzaib Shaikh <shahzaibshaikh98@gmail.com>
2023-05-16 22:08:06 +0500

    🐛 FIX: resolved layout bug

--- app/restaurant/[slug]/layout.tsx
+++ app/restaurant/[slug]/layout.tsx
@@ -11,7 +11,9 @@ function RestaurantLayout({
   return (
     <main>
       <Header name={params.slug} />
-      <div className='flex m-auto w-2/3 justify-between items-start 0 -mt-11'></div>
+      <div className='flex m-auto w-2/3 justify-between items-start 0 -mt-11'>
+        {children}
+      </div>
     </main>
   );
 }
