8eb13e28310f27f3a2d5e797b9970e0220fbf0ce
Shahzaib Shaikh <shahzaibshaikh98@gmail.com>
2023-05-17 22:44:30 +0500

    👌 IMPROVE: added renderTitle utility function

--- app/restaurant/[slug]/components/Header.tsx
+++ app/restaurant/[slug]/components/Header.tsx
@@ -1,6 +1,12 @@
 import React from 'react';
 
 function Header({ name }: { name: string }) {
+  const renderTitle = () => {
+    const name_array = name.split('-');
+    name_array[name_array.length - 1] = `(${name_array[name_array.length - 1]})`;
+    return name_array.join(' ');
+  };
+
   return (
     <div className='h-96 overflow-hidden'>
       <div className='bg-center bg-gradient-to-r from-[#0f1f47] to-[#5f6984] h-full flex justify-center items-center'>
