346f91933400f4503cdd3dc52ce5d76b0ce41a7e
Shahzaib Shaikh <shahzaibshaikh98@gmail.com>
2023-05-17 22:50:15 +0500

    👌 IMPROVE: modified fetchItems function

--- app/restaurant/[slug]/menu/page.tsx
+++ app/restaurant/[slug]/menu/page.tsx
@@ -10,8 +10,17 @@ const prisma = new PrismaClient();
 
 const fetchItems = async (slug: string) => {
   const restaurant = await prisma.restaurant.findUnique({
-    where: { slug }
+    where: { slug },
+    select: {
+      items: true
+    }
   });
+
+  if (!restaurant) {
+    throw new Error();
+  }
+
+  return restaurant.items;
 };
 
 function RestaurantMenu({ params }: { params: { slug: string } }) {
