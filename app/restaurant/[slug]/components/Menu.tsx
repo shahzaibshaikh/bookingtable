d786298e84c8cc3a522f30f5d2bcbe897eed34fc
Shahzaib Shaikh <shahzaibshaikh98@gmail.com>
2023-05-17 23:07:47 +0500

    👌 IMPROVE: added no menu fallback

--- app/restaurant/[slug]/components/Menu.tsx
+++ app/restaurant/[slug]/components/Menu.tsx
@@ -9,11 +9,17 @@ function Menu({ menu }: { menu: Item[] }) {
         <div className='mt-4 pb-1 mb-1'>
           <h1 className='font-bold text-4xl'>Menu</h1>
         </div>
-        <div className='flex flex-wrap justify-between'>
-          {menu.map(item => (
-            <MenuCard key={item.id} item={item} />
-          ))}
-        </div>
+        {menu.length ? (
+          <div className='flex flex-wrap justify-between'>
+            {menu.map(item => (
+              <MenuCard key={item.id} item={item} />
+            ))}
+          </div>
+        ) : (
+          <div className='flex flex-wrap justify-between'>
+            <p>This restaurant does not have a menu.</p>
+          </div>
+        )}
       </div>
     </main>
   );
