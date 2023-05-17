960df524bb7f024557ae19e2ded6e59a94e2ff67
Shahzaib Shaikh <shahzaibshaikh98@gmail.com>
2023-05-17 22:46:07 +0500

    👌 IMPROVE: changed to capital case

--- app/restaurant/[slug]/components/Header.tsx
+++ app/restaurant/[slug]/components/Header.tsx
@@ -10,7 +10,7 @@ function Header({ name }: { name: string }) {
   return (
     <div className='h-96 overflow-hidden'>
       <div className='bg-center bg-gradient-to-r from-[#0f1f47] to-[#5f6984] h-full flex justify-center items-center'>
-        <h1 className='text-7xl text-white captitalize text-shadow text-center'>
+        <h1 className='text-7xl text-white capitalize text-shadow text-center'>
           {renderTitle()}
         </h1>
       </div>
