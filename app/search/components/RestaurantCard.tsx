2002eb437b2efe0a02b9307e49b61181a0667672
Shahzaib Shaikh <shahzaibshaikh98@gmail.com>
2023-05-27 20:29:55 +0500

    👌 IMPROVE: added price component

--- app/search/components/RestaurantCard.tsx
+++ app/search/components/RestaurantCard.tsx
@@ -1,3 +1,4 @@
+import Price from '@/app/components/Price';
 import { Cuisine, Location, PRICE } from '@prisma/client';
 import Link from 'next/link';
 import React from 'react';
@@ -21,14 +22,14 @@ function RestaurantCard({ restaurant }: { restaurant: Restaurant }) {
         className='w-44 rounded'
       />
       <div className='pl-5'>
-        <h2 className='text-3xl'>Aiāna Restaurant Collective</h2>
+        <h2 className='text-3xl'>{restaurant.name}</h2>
         <div className='flex items-start'>
           <div className='flex mb-2'>*****</div>
           <p className='ml-2 text-sm'>Awesome</p>
         </div>
         <div className='mb-9'>
           <div className='font-light flex text-reg'>
-            <p className='mr-4'>$$$</p>
+            <Price price={restaurant.price} />
             <p className='mr-4'>Mexican</p>
             <p className='mr-4'>Ottawa</p>
           </div>
