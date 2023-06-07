8cb0e729b17a155c83259562818f537391b4e084
Shahzaib Shaikh <shahzaibshaikh98@gmail.com>
2023-06-07 22:11:47 +0500

    🐛 FIX: added cap class

--- app/search/components/RestaurantCard.tsx
+++ app/search/components/RestaurantCard.tsx
@@ -26,8 +26,8 @@ function RestaurantCard({ restaurant }: { restaurant: Restaurant }) {
         <div className='mb-9'>
           <div className='font-light flex text-reg'>
             <Price price={restaurant.price} />
-            <p className='mr-4 '>{restaurant.cuisine.name}</p>
-            <p className='mr-4 '>{restaurant.location.name}</p>
+            <p className='mr-4 capitalize'>{restaurant.cuisine.name}</p>
+            <p className='mr-4 capitalize'>{restaurant.location.name}</p>
           </div>
         </div>
         <div className='text-red-600'>
