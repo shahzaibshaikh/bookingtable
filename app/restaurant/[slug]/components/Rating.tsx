4729f48e8b2fbaaa2939398acde30273d8613526
Shahzaib Shaikh <shahzaibshaikh98@gmail.com>
2023-06-09 00:05:12 +0500

    👌 IMPROVE: added stars to restaurant page

--- app/restaurant/[slug]/components/Rating.tsx
+++ app/restaurant/[slug]/components/Rating.tsx
@@ -1,3 +1,4 @@
+import Stars from '@/app/components/Stars';
 import { calculateReviewRatingAverage } from '@/utils/calculateReviewRatingAverage';
 import { Review } from '@prisma/client';
 import React from 'react';
@@ -6,7 +7,7 @@ function Rating({ reviews }: { reviews: Review[] }) {
   return (
     <div className='flex items-end'>
       <div className='ratings mt-2 flex items-center'>
-        <p>*****</p>
+        <Stars reviews={reviews} />
         <p className='text-reg ml-3'>
           {calculateReviewRatingAverage(reviews).toFixed(1)}
         </p>
