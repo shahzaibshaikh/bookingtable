4dfeb803a18174269b93339b90ac928d96991067
Shahzaib Shaikh <shahzaibshaikh98@gmail.com>
2023-06-08 22:28:46 +0500

    👌 IMPROVE: rendered review header

--- app/restaurant/[slug]/page.tsx
+++ app/restaurant/[slug]/page.tsx
@@ -48,7 +48,7 @@ async function RestaurantDetail({ params }: { params: { slug: string } }) {
           <div className='bg-white w-[70%] rounded p-3 shadow'>
             <RestaurantNavbar slug={restaurant?.slug} />
             <Title name={restaurant?.name} />
-            <Rating />
+            <Rating reviews={restaurant.reviews} />
             <Description description={restaurant?.description} />
             <Images images={restaurant.images} />
             <Reviews reviews={restaurant.reviews} />
