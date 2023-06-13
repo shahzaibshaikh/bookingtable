4b09e587801fee03457449b30eb083c2abd2a387
Shahzaib Shaikh <shahzaibshaikh98@gmail.com>
2023-06-13 22:38:12 +0500

    👌 IMPROVE: added firstname and lastname validatio

--- app/api/auth/signup/route.ts
+++ app/api/auth/signup/route.ts
@@ -1,8 +1,24 @@
 import { NextRequest, NextResponse } from 'next/server';
+import validator from 'validator';
 
 export async function POST(req: NextRequest) {
-  const body = await req.json();
-  console.log(body);
+  const { firstname, lastname } = await req.json();
+  const validationSchema = [
+    {
+      valid: validator.isLength(firstname, {
+        min: 1,
+        max: 25
+      }),
+      errorMessage: 'Firstname is invalid'
+    },
+    {
+      valid: validator.isLength(lastname, {
+        min: 1,
+        max: 25
+      }),
+      errorMessage: 'Firstname is invalid'
+    }
+  ];
 
-  return NextResponse.json(body, { status: 200 });
+  return NextResponse.json('body', { status: 200 });
 }
