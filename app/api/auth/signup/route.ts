fcd9361690db6b9df546eef2f3a97c2ad3db3c44
Shahzaib Shaikh <shahzaibshaikh98@gmail.com>
2023-06-13 22:30:58 +0500

    👌 IMPROVE: change response method

--- app/api/auth/signup/route.ts
+++ app/api/auth/signup/route.ts
@@ -3,6 +3,4 @@ import { NextRequest, NextResponse } from 'next/server';
 export async function POST(req: NextRequest) {
   const body = req.body;
   console.log(body);
-
-  return NextResponse.json(body);
 }
