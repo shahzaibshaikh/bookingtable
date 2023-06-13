3ab01351876cc6a0b61083c5c6369f5fdc6aa60e
Shahzaib Shaikh <shahzaibshaikh98@gmail.com>
2023-06-13 22:33:44 +0500

    👌 IMPROVE: added status code

--- app/api/auth/signup/route.ts
+++ app/api/auth/signup/route.ts
@@ -4,5 +4,5 @@ export async function POST(req: NextRequest) {
   const body = await req.json();
   console.log(body);
 
-  return NextResponse.json(body);
+  return NextResponse.json(body, { status: 200 });
 }
