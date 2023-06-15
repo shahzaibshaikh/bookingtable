698aa42f3f3253221a5c307a2db83d5570e7c114
Shahzaib Shaikh <shahzaibshaikh98@gmail.com>
2023-06-15 23:10:09 +0500

    👌 IMPROVE: returned just the token

--- app/api/auth/me/route.ts
+++ app/api/auth/me/route.ts
@@ -1,6 +1,10 @@
 import { NextRequest, NextResponse } from 'next/server';
 
 export async function POST(req: NextRequest) {
-  const bearerToken = req.headers.get('authorization') as string;
+  const bearerToken = req.headers.get('authorization')?.split(' ')[1] as string;
+  if (!bearerToken) {
+    return NextResponse.json({ error: 'Unauthorized request' }, { status: 401 });
+  }
+
   return NextResponse.json(bearerToken);
 }
