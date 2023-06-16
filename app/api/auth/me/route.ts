12a02fc6361a0e25172fe6c82489d1a81410a716
Shahzaib Shaikh <shahzaibshaikh98@gmail.com>
2023-06-16 18:46:45 +0500

    👌 IMPROVE: retrieved user from DB using token

--- app/api/auth/me/route.ts
+++ app/api/auth/me/route.ts
@@ -1,5 +1,9 @@
 import { NextRequest, NextResponse } from 'next/server';
 import * as jose from 'jose';
+import jwt from 'jsonwebtoken';
+import { PrismaClient } from '@prisma/client';
+
+const prisma = new PrismaClient();
 
 export async function POST(req: NextRequest) {
   const token = req.headers.get('authorization')?.split(' ')[1] as string;
@@ -15,5 +19,17 @@ export async function POST(req: NextRequest) {
     return NextResponse.json({ error: 'Unauthorized request' }, { status: 401 });
   }
 
-  return NextResponse.json(token);
+  const payload = jwt.decode(token) as { email: string };
+
+  if (!payload.email) {
+    return NextResponse.json({ error: 'Unauthorized request' }, { status: 401 });
+  }
+
+  const user = await prisma.user.findUnique({
+    where: {
+      email: payload?.email
+    }
+  });
+
+  return NextResponse.json(user);
 }
