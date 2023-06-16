f014d4c1d502be7234a936bff0358bf7c70e75eb
Shahzaib Shaikh <shahzaibshaikh98@gmail.com>
2023-06-16 19:16:46 +0500

    👌 IMPROVE: completed middleware function

--- app/api/auth/me/route.ts
+++ app/api/auth/me/route.ts
@@ -7,17 +7,6 @@ const prisma = new PrismaClient();
 
 export async function POST(req: NextRequest) {
   const token = req.headers.get('authorization')?.split(' ')[1] as string;
-  if (!token) {
-    return NextResponse.json({ error: 'No bearer token' }, { status: 401 });
-  }
-
-  const secret = new TextEncoder().encode(process.env.JWT_SECRET_KEY);
-
-  try {
-    await jose.jwtVerify(token, secret);
-  } catch (error) {
-    return NextResponse.json({ error: 'Unauthorized request' }, { status: 401 });
-  }
 
   const payload = jwt.decode(token) as { email: string };
 
--- middleware.ts
+++ middleware.ts
@@ -1,7 +1,19 @@
 import { NextRequest, NextResponse } from 'next/server';
+import * as jose from 'jose';
 
 export async function middleware(req: NextRequest) {
-  console.log('Hello I am middlware.');
+  const token = req.headers.get('authorization')?.split(' ')[1] as string;
+  if (!token) {
+    return NextResponse.json({ error: 'No bearer token' }, { status: 401 });
+  }
+
+  const secret = new TextEncoder().encode(process.env.JWT_SECRET_KEY);
+
+  try {
+    await jose.jwtVerify(token, secret);
+  } catch (error) {
+    return NextResponse.json({ error: 'Unauthorized request' }, { status: 401 });
+  }
 }
 
 export const config = {
