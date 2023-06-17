da5616fddb094440ace0f5a748bc1cdba8a8efde
Shahzaib Shaikh <shahzaibshaikh98@gmail.com>
2023-06-17 18:58:07 +0500

    📦 NEW: added auth contect file

---
+++ app/context/AuthContext.tsx
@@ -0,0 +1,34 @@
+'use client';
+import React, { createContext, useState } from 'react';
+
+interface User {
+  id: number;
+  firstname: string;
+  lastname: string;
+  email: string;
+  city: string;
+  phone: string;
+}
+
+interface AuthState {
+  loading: boolean;
+  data: string | null;
+  error: User | null;
+}
+
+const AuthenticationContext = createContext<AuthState>({
+  loading: false,
+  data: null,
+  error: null
+});
+
+function AuthContext({ children }: { children: React.ReactNode }) {
+  const [authState, setAuthState] = useState({
+    loading: false,
+    data: null,
+    error: null
+  });
+  return <div>{children}</div>;
+}
+
+export default AuthContext;
