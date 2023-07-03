fda8c8ab52f70918ea7fc4323e4f12557c999de7
Shahzaib Shaikh <shahzaibshaikh98@gmail.com>
2023-07-03 12:21:07 +0500

    📦 NEW: defined the signup function

--- hooks/useAuth.ts
+++ hooks/useAuth.ts
@@ -35,7 +35,49 @@ const useAuth = () => {
       });
     }
   };
-  const signup = async () => {};
+  const signup = async (
+    {
+      email,
+      password,
+      firstname,
+      lastname,
+      city,
+      phone
+    }: {
+      email: string;
+      password: string;
+      firstname: string;
+      lastname: string;
+      city: string;
+      phone: string;
+    },
+    handleClose: () => void
+  ) => {
+    setAuthState({
+      data: null,
+      error: null,
+      loading: true
+    });
+    try {
+      const response = await axios.post('http://localhost:3002/api/auth/login', {
+        email,
+        password
+      });
+      setAuthState({
+        data: response.data,
+        error: null,
+        loading: false
+      });
+      setCookie('jwt', response.data.token);
+      handleClose();
+    } catch (error: any) {
+      setAuthState({
+        data: null,
+        error: error.response.data.error,
+        loading: false
+      });
+    }
+  };
 
   return { signin, signup };
 };
